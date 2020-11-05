aec86a887fa013a494c3946ec1771464bd30d830
Isaac Terry <zeroice2007@gmail.com>
2020-11-05 12:39:41 -0600

    feat: add ability to edit profile

---
+++ scripts/index.js
@@ -0,0 +1,45 @@
+function handleEditProfile() {
+  let profileEditSection = document.querySelector('.profile-edit');
+  profileEditSection.classList.remove('profile-edit_hidden');
+}
+
+function handleSaveProfile(event) {
+  event.preventDefault();
+
+  // retrieve elements that have user input
+  let form = document.querySelector('.profile-edit__form');
+  let nameInputElement = form.querySelector('.profile-edit__form-input_type_name');
+  let aboutMeInputElement = form.querySelector('.profile-edit__form-input_type_about-me');
+
+  // retreive elements where the user input will go
+  let profile = document.querySelector('.profile');
+  let profileName = profile.querySelector('.profile__name');
+  let profileSelfDescription = profile.querySelector('.profile__self-description');
+
+  // place user input where it needs to go
+  profileName.textContent = nameInputElement.value;
+  profileSelfDescription.textContent = aboutMeInputElement.value;
+
+  handleCloseProfileForm();
+}
+
+function handleCloseProfileForm() {
+  let form = document.querySelector('.profile-edit__form');
+  let nameInputElement = form.querySelector('.profile-edit__form-input_type_name');
+  let aboutMeInputElement = form.querySelector('.profile-edit__form-input_type_about-me');
+
+  // close and reset form
+  nameInputElement.value = '';
+  aboutMeInputElement.value = '';
+
+  let profileEditSection = document.querySelector('.profile-edit');
+  profileEditSection.classList.add('profile-edit_hidden');
+}
+
+let editButton = document.querySelector('.profile__edit-btn');
+let saveButton = document.querySelector('.profile-edit__form-button');
+let closeIcon = document.querySelector('.profile-edit__close-icon');
+
+editButton.addEventListener('click', handleEditProfile);
+saveButton.addEventListener('click', handleSaveProfile);
+closeIcon.addEventListener('click', handleCloseProfileForm);
