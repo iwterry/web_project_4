8619024b33818426c29c454bed852562e921f1fb
Isaac Terry <zeroice2007@gmail.com>
2020-11-26 19:31:50 -0600

    feature: add ability add locations and display popup of image click by user

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,26 @@
-const profileEditSection = document.querySelector('.overlay');
-const closeButton = document.querySelector('.overlay__close-btn');
+// ########### defining variable #########
+const closeButtons = document.querySelectorAll('.overlay__close-btn');
 
-const form = document.querySelector('.web-project-four-form');
-const nameInputElement = form.querySelector('.web-project-four-form__input_type_profile-name');
-const aboutMeInputElement = form.querySelector('.web-project-four-form__input_type_profile-about-me');
+const locationPopupImage = document.querySelector('.image-popup__image');
+const locationPopupImageTitle = document.querySelector('.image-popup__title');
+
+const profileEditForm = document.querySelector('.web-project-four-form_type_profile-edit');
+const profileEditNameInput = profileEditForm
+  .querySelector('.web-project-four-form__input_type_profile-name');
+const profileEditAboutMeInput = profileEditForm
+  .querySelector('.web-project-four-form__input_type_profile-about-me');
+
+const locationCreationForm = document.querySelector('.web-project-four-form_type_location-create');
+const locationCreationTitle = locationCreationForm
+  .querySelector('.web-project-four-form__input_type_location-title');
+const locationCreationImageLink = locationCreationForm
+  .querySelector('.web-project-four-form__input_type_location-image-link');
+
 
 const profileName = document.querySelector('.profile__name');
 const profileSelfDescription = document.querySelector('.profile__self-description');
 const editButton = document.querySelector('.profile__edit-btn');
+const addButton = document.querySelector('.profile__add-btn');
 
 const locationsCollection = document.querySelector('.locations__collection');
 const locationTemplate = document.querySelector('#location').content;
@@ -41,13 +54,59 @@ const initialLocations = [
 
 
 // ####### defining event handlers #######
+// ------ handlers / helpers dealing with overlay
+function getOverlay(childOfOrIsAnOverlay) {
+  return childOfOrIsAnOverlay.closest('.overlay');
+}
+
+function closeOverlay(childOfOrIsAnOverlay) {
+  getOverlay(childOfOrIsAnOverlay).classList.remove('overlay_opened');
+ }
+ 
+ function openOverlay(childOfOrIsAnOverlay) {
+   getOverlay(childOfOrIsAnOverlay).classList.add('overlay_opened');
+ }
+
+function handleCloseOverlay(evt) {
+  const overlayCloseButton = evt.target;
+  closeOverlay(overlayCloseButton);
+}
+
+// ------ handlers dealing with editing profile
 function handleEditProfile() {
   // fill in input values with the given text content of the profile
-  nameInputElement.value = profileName.textContent;
-  aboutMeInputElement.value = profileSelfDescription.textContent;
+  profileEditNameInput.value = profileName.textContent;
+  profileEditAboutMeInput.value = profileSelfDescription.textContent;
   
   // show the form
-  profileEditSection.classList.add('overlay_opened');
+  openOverlay(profileEditForm);
+}
+
+function handleSaveProfile(evt) {
+  evt.preventDefault();
+
+  // fill in profile information from user input
+  profileName.textContent = profileEditNameInput.value;
+  profileSelfDescription.textContent = profileEditAboutMeInput.value;
+
+  // hide the form
+  closeOverlay(profileEditForm);
+}
+
+// -------- handlers / helpers dealing with location
+function handleAddLocation() {
+  openOverlay(locationCreationForm);
+}
+
+function handleSaveLocation(evt) {
+  evt.preventDefault();
+
+  addLocation({
+    name: locationCreationTitle.value,
+    link: locationCreationImageLink.value
+  });
+
+  closeOverlay(locationCreationForm);
 }
 
 function handleDeleteLocation(evt) {
@@ -60,37 +119,39 @@ function handleLikeLocation(evt) {
   eventTarget.classList.toggle('location__like-btn_active');
 }
 
-function handleSaveProfile(event) {
-  event.preventDefault();
-
-  // fill in profile information from user input
-  profileName.textContent = nameInputElement.value;
-  profileSelfDescription.textContent = aboutMeInputElement.value;
-
-  // hide the form
-  handleCloseProfileForm();
-}
-
-function handleCloseProfileForm() {
-  profileEditSection.classList.remove('overlay_opened');
-}
-
-// ###### helper functions ########
 function addLocation({ name: locationName, link: locationImageLink }) {
   const locationElement = locationTemplate.cloneNode(true);
+  const locationImage = locationElement.querySelector('.location__image');
 
+  locationImage.src = locationImageLink;
+  locationImage.alt = locationName;
   locationElement.querySelector('.location__name').textContent = locationName;
-  locationElement.querySelector('.location__image').src = locationImageLink;
+
   locationElement.querySelector('.location__delete-btn').addEventListener('click', handleDeleteLocation);
   locationElement.querySelector('.location__like-btn').addEventListener('click', handleLikeLocation);
-  locationsCollection.append(locationElement);
+  locationElement.querySelector('.location__image').addEventListener('click', handleSelectImage);
+
+  locationsCollection.prepend(locationElement);
 }
 
+// ------ handlers dealing with selecting popup image
+function handleSelectImage(evt) {
+  const locationImage = evt.target;
 
-// add initial locations to the DOM
+  locationPopupImage.src = locationImage.src;
+  locationPopupImage.alt = locationImage.alt;
+  locationPopupImageTitle.textContent = locationImage.alt;
+
+  locationPopupImage.closest('.overlay').classList.add('overlay_opened');
+}
+
+// ######### adding locations and handlers
+// ------ add initial locations to the DOM
 initialLocations.forEach(addLocation);
 
-// ####### adding event handlers #######
+//---- adding event handlers
 editButton.addEventListener('click', handleEditProfile);
-form.addEventListener('submit', handleSaveProfile);
-closeButton.addEventListener('click', handleCloseProfileForm);
+profileEditForm.addEventListener('submit', handleSaveProfile);
+addButton.addEventListener('click', handleAddLocation);
+locationCreationForm.addEventListener('submit', handleSaveLocation);
+closeButtons.forEach((closeButton) => closeButton.addEventListener('click', handleCloseOverlay));
