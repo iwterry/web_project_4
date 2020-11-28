04c4a5827e9d661a86a6393b7a5ca37f477957b9
Isaac Terry <zeroice2007@gmail.com>
2020-11-27 20:21:06 -0600

    fix: refactor separate initial card data into a separate js file

--- scripts/index.js
+++ scripts/index.js
@@ -24,33 +24,6 @@ const addButton = document.querySelector('.profile__add-btn');
 const cardsCollection = document.querySelector('.locations__collection');
 const cardTemplate = document.querySelector('#location').content;
 
-const initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg"
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg"
-  }
-]; 
-
 
 // ####### defining event handlers #######
 // ------ handlers / helpers dealing with overlay
@@ -149,9 +122,10 @@ function handleSelectImage(evt) {
   showPopup(cardPopupImage);
 }
 
+
 // ######### adding cards and handlers #######
 // ------ add initial cards to the DOM
-initialCards.forEach((cardObj) => addCardToDom(getNewCardElement(cardObj)));
+initialCardObjs.forEach((cardObj) => addCardToDom(getNewCardElement(cardObj)));
 
 //---- adding event handlers
 editButton.addEventListener('click', handleEditProfile);
