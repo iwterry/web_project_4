e217ebb97708d86cf11e7faec291e3a87c3df8fe
Isaac Terry <zeroice2007@gmail.com>
2020-12-23 19:10:01 -0600

    refactor: add IFFE  around block of code for index.js

--- scripts/index.js
+++ scripts/index.js
@@ -9,115 +9,117 @@ import {
   showCardImageWithPopup
 } from './popup-util.js';
 
-// ########### defining DOM element variables #########
-const overlays = document.querySelectorAll('.overlay');
-const closeButtons = document.querySelectorAll('.overlay__close-btn');
+(function main() {
+  // ########### defining DOM element variables #########
+  const overlays = document.querySelectorAll('.overlay');
+  const closeButtons = document.querySelectorAll('.overlay__close-btn');
 
-const profileName = document.querySelector('.profile__name');
-const profileSelfDescription = document.querySelector('.profile__self-description');
-const editButton = document.querySelector('.profile__edit-btn');
-const addButton = document.querySelector('.profile__add-btn');
+  const profileName = document.querySelector('.profile__name');
+  const profileSelfDescription = document.querySelector('.profile__self-description');
+  const editButton = document.querySelector('.profile__edit-btn');
+  const addButton = document.querySelector('.profile__add-btn');
 
-const profileEditForm = document.forms.profileEditForm;
-const profileEditNameInput = profileEditForm.elements.profileName;
-const profileEditAboutMeInput = profileEditForm.elements.aboutMe;
+  const profileEditForm = document.forms.profileEditForm;
+  const profileEditNameInput = profileEditForm.elements.profileName;
+  const profileEditAboutMeInput = profileEditForm.elements.aboutMe;
 
-const cardCreationForm = document.forms.locationCreateForm;
-const cardCreationTitleInput = cardCreationForm.elements.locationTitle;
-const cardCreationImageLinkInput = cardCreationForm.elements.imageLink;
+  const cardCreationForm = document.forms.locationCreateForm;
+  const cardCreationTitleInput = cardCreationForm.elements.locationTitle;
+  const cardCreationImageLinkInput = cardCreationForm.elements.imageLink;
 
-const cardsCollection = document.querySelector('.locations__collection');
-const formElements = document.querySelectorAll('.project-form');
+  const cardsCollection = document.querySelector('.locations__collection');
+  const formElements = document.querySelectorAll('.project-form');
 
-// ####### defining event handlers ######
-// ------ handlers dealing with editing profile
-function handleShowProfileEditForm() {
-  // fill in input values with the given text content of the profile
-  profileEditNameInput.value = profileName.textContent;
-  profileEditAboutMeInput.value = profileSelfDescription.textContent;
-  
-  // show the form
-  showPopup(profileEditForm);
-}
+  // ####### defining event handlers ######
+  // ------ handlers dealing with editing profile
+  function handleShowProfileEditForm() {
+    // fill in input values with the given text content of the profile
+    profileEditNameInput.value = profileName.textContent;
+    profileEditAboutMeInput.value = profileSelfDescription.textContent;
+    
+    // show the form
+    showPopup(profileEditForm);
+  }
 
-function handleSaveProfile(evt) {
-  evt.preventDefault();
+  function handleSaveProfile(evt) {
+    evt.preventDefault();
 
-  // fill in profile information from user input
-  profileName.textContent = profileEditNameInput.value;
-  profileSelfDescription.textContent = profileEditAboutMeInput.value;
+    // fill in profile information from user input
+    profileName.textContent = profileEditNameInput.value;
+    profileSelfDescription.textContent = profileEditAboutMeInput.value;
 
-  // hide the form
-  hidePopup(profileEditForm);
-}
+    // hide the form
+    hidePopup(profileEditForm);
+  }
 
-// -------- handlers / helpers dealing with card
-function getNewCardElement(cardDataObj) {
-  const cardTemplateSelector = '#location';
-  const newCard = new Card(cardDataObj, cardTemplateSelector, showCardImageWithPopup);
+  // -------- handlers / helpers dealing with card
+  function getNewCardElement(cardDataObj) {
+    const cardTemplateSelector = '#location';
+    const newCard = new Card(cardDataObj, cardTemplateSelector, showCardImageWithPopup);
 
-  return newCard.generateNewCardElement();
-}
+    return newCard.generateNewCardElement();
+  }
 
-function addCardToDom(cardElement) {
-  cardsCollection.prepend(cardElement);
-}
+  function addCardToDom(cardElement) {
+    cardsCollection.prepend(cardElement);
+  }
 
-function handleShowCardCreationForm() {
-  showPopup(cardCreationForm);
-}
+  function handleShowCardCreationForm() {
+    showPopup(cardCreationForm);
+  }
 
-function handleSaveCard(evt) {
-  evt.preventDefault();
+  function handleSaveCard(evt) {
+    evt.preventDefault();
 
-  const newCardElement = getNewCardElement({
-    name: cardCreationTitleInput.value,
-    link: cardCreationImageLinkInput.value
-  });
+    const newCardElement = getNewCardElement({
+      name: cardCreationTitleInput.value,
+      link: cardCreationImageLinkInput.value
+    });
 
-  addCardToDom(newCardElement);
-  hidePopup(cardCreationForm);
-}
-
-  // ------ adding data for cards, events listeners dealing with forms, and form validation --------
-function addInitialCardsToDom() {
-  initialCardObjs.forEach(function (cardDataObj) {
-    const newCardElement = getNewCardElement(cardDataObj);
     addCardToDom(newCardElement);
-  });
-}
+    hidePopup(cardCreationForm);
+  }
 
-function addInitialEventListeners() {
-  // for buttons to show form
-  editButton.addEventListener('click', handleShowProfileEditForm);
-  addButton.addEventListener('click', handleShowCardCreationForm);
-  // for forms
-  profileEditForm.addEventListener('submit', handleSaveProfile);
-  cardCreationForm.addEventListener('submit', handleSaveCard);
-  // for popup closing functionality
-  closeButtons.forEach((closeButton) => closeButton.addEventListener('click', handleHidePopup));
-  overlays.forEach((overlay) => overlay.addEventListener(
-    'click', 
-    (evt) => hidePopupByClickingOnOverlay(evt.target, overlay)
-  ));
-}
+    // ------ adding data for cards, events listeners dealing with forms, and form validation --------
+  function addInitialCardsToDom() {
+    initialCardObjs.forEach(function (cardDataObj) {
+      const newCardElement = getNewCardElement(cardDataObj);
+      addCardToDom(newCardElement);
+    });
+  }
 
-function addFormValidation() {
-  const settingsObj = {
-    inputSelector: '.project-form__input',
-    inputErrorMsgSelectorPrefix: '.project-form__input-error_field_',
-    submitBtnSelector: '.project-form__submit-btn',
-    disabledButtonClass: 'project-form__submit-btn_disabled',
-    inputErrorClass: 'project-form__input_type_error',
-    errorMsgVisibilityClass: 'project-form__input-error_active',
-  };
+  function addInitialEventListeners() {
+    // for buttons to show form
+    editButton.addEventListener('click', handleShowProfileEditForm);
+    addButton.addEventListener('click', handleShowCardCreationForm);
+    // for forms
+    profileEditForm.addEventListener('submit', handleSaveProfile);
+    cardCreationForm.addEventListener('submit', handleSaveCard);
+    // for popup closing functionality
+    closeButtons.forEach((closeButton) => closeButton.addEventListener('click', handleHidePopup));
+    overlays.forEach((overlay) => overlay.addEventListener(
+      'click', 
+      (evt) => hidePopupByClickingOnOverlay(evt.target, overlay)
+    ));
+  }
 
-  formElements.forEach(function (formElement) {
-    const formValidator = new FormValidator(settingsObj, formElement);
-    formValidator.enableValidation();
-  });
-}
+  function addFormValidation() {
+    const settingsObj = {
+      inputSelector: '.project-form__input',
+      inputErrorMsgSelectorPrefix: '.project-form__input-error_field_',
+      submitBtnSelector: '.project-form__submit-btn',
+      disabledButtonClass: 'project-form__submit-btn_disabled',
+      inputErrorClass: 'project-form__input_type_error',
+      errorMsgVisibilityClass: 'project-form__input-error_active',
+    };
 
-addInitialCardsToDom();
-addInitialEventListeners();
-addFormValidation();
+    formElements.forEach(function (formElement) {
+      const formValidator = new FormValidator(settingsObj, formElement);
+      formValidator.enableValidation();
+    });
+  }
+
+  addInitialCardsToDom();
+  addInitialEventListeners();
+  addFormValidation();
+})();
